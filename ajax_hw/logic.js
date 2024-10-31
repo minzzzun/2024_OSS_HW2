4cce15db606d59244c8cd8b63b57c64f5ed7535d
minjun kim <minzzzun@gmail.com>
2024-10-31 22:50:55 +0900

    [Feat] : PUT, DELETE 기능추가

--- ajax_hw/logic.js
+++ ajax_hw/logic.js
@@ -10,7 +10,8 @@ function getData(){
         console.log(xhr.responseText);  
 
         let books = JSON.parse(xhr.responseText);
-        
+        sessionStorage.setItem("booksData", JSON.stringify(books));
+
         let tableBody = document.getElementById("bookTable");
         $("#bookTable").html("");
 
@@ -18,7 +19,7 @@ function getData(){
           const row = `
             <tr>
               <th scope="row">${book.count}</th>
-              <td class="td_title" onclick="location.href='./view_server.html'">${book.title}</td>
+              <td class="td_title" onclick="viewBookDetails('${book.id}')">${book.title}</td>
               <td>${book.author}</td>
               <td>${book.publisher}</td>
               <td>${book.year}</td>
@@ -33,6 +34,10 @@ function getData(){
 
 }
 
+function viewBookDetails(bookId) {
+    sessionStorage.setItem("selectedBookId", bookId);
+    location.href = "./view_server.html";
+  }
 
 function postData(){
 
@@ -59,24 +64,65 @@ function postData(){
 
 }
 
+function buttonClicked(){
+  const selectedBookId = sessionStorage.getItem("selectedBookId");
+  updateData(selectedBookId)
+}
+
+function updateData(id){
+  let count = $("#edit_book_number").val();
+  let title = $("#edit_book_title").val();
+  let author = $("#edit_book_author").val();
+  let publisher = $("#edit_book_pub").val();
+  let year = $("#edit_book_year").val();
+
+  let newBook = {
+      count: parseInt(count),
+      title: title,
+      author: author,
+      publisher: publisher,
+      year: parseInt(year)
+  };
+
+  fetch(`http://localhost:3000/books/${id}`, {
+  method: "PUT",
+  headers: {
+    "Content-Type": "application/json"
+  },
+   body: JSON.stringify(newBook) 
+  })
+    .then(response => {
+    if (response.ok) {
+      alert("도서 정보가 성공적으로 수정되었습니다.");
+    } else {
+      alert("도서 정보를 수정하는 데 실패했습니다.");
+    }
+  })
+  .catch(error => console.error("Error:", error));
+}
+
+function deleteButtonClicked(){
+  const selectedBookId = sessionStorage.getItem("selectedBookId");
+  deleteData(selectedBookId)
+  console.log(selectedBookId)
+}
 
 
 
+function deleteData(id){
+  fetch(`http://localhost:3000/books/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      alert("도서 정보가 성공적으로 삭제되었습니다.");
+    } else {
+      alert("도서 정보를 삭제하는 데 실패했습니다.");
+    }
+  })
+  .catch(error => console.error("Error:", error));
 
-
-function bbb(){
-
-    const xhr = new XMLHttpRequest(); 
-    xhr.open("POST", "http://localhost:3000/students"); 
-    xhr.setRequestHeader("content-type", "application/json;chatset=UTF-8");
-
-    let data = {name:'name1', age:12};
-
-    xhr.send(JSON.stringify(data));
-    xhr.onload = () => { 
-        if(xhr.status === 201){
-            alert(JSON.stringify(xhr.response));
-        }
-        //document.getElementById("demo").innerHTML = this.responseText; 
-    } 
 }
